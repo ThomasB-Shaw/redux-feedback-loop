89d45b20333412f63994567bbd97eea477739081
Thomas Brookshaw <tbrookshaw13@gmail.com>
2020-10-30 19:35:25 -0500

    initial commit, intalls complete, todo set up, index.js provider added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const storeInstance = createStore(
+    combineReducers({
+    }),
+  );
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();

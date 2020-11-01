c29083f216e425367d92abd08391f834457561d1
Thomas Brookshaw <tbrookshaw13@gmail.com>
2020-11-01 15:12:34 -0600

    added delete route in feedbackRoter.js

--- server/routes/feedbackRouter.js
+++ server/routes/feedbackRouter.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const pool = require('../modules/pool');
 
+// ERROR IN POST
 router.post('/',  (req, res) => {
     let feedback = req.body;
     console.log(`Adding Feedback`, feedback);
@@ -12,6 +13,7 @@ router.post('/',  (req, res) => {
       .then((result) => {
         res.sendStatus(201);
         console.log(result);
+
       })
       .catch((error) => {
         console.log(`Error adding feedback`, error);
@@ -29,4 +31,15 @@ router.post('/',  (req, res) => {
       res.sendStatus(500);
     });
   });
+
+  // DELETE FEEDBACK IN ADMIN
+router.delete('/:id', (req, res) => {
+    pool.query('DELETE FROM "feedback" WHERE id=$1', [req.params.id]).then((result) => {
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log('Error DELETE feedback', error);
+        res.sendStatus(500);
+    })
+});
+
 module.exports = router;
